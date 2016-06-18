9dbd25b2268ccc60a5498537f422da493320b3fb
Chris Gerth <chrisgerth010592@gmail.com>
2016-06-17 23:35:50 -0500

    Migrated updated integral calcualtor from alanis's work in the casserole2016 repo

--- java/src/org/usfirst/frc/team1736/lib/SignalMath/IntegralCalculator.java
+++ java/src/org/usfirst/frc/team1736/lib/SignalMath/IntegralCalculator.java
@@ -1,64 +1,52 @@
-package org.usfirst.frc.team1736.lib.SignalMath;
+package org.usfirst.frc.team1736.robot;
 
 import edu.wpi.first.wpilibj.Timer;
-
-///////////////////////////////////////////////////////////////////////////////
-// Copyright (c) FRC Team 1736 2016. See the License file. 
-//
-// Can you use this code? Sure! We're releasing this under GNUV3, which 
-// basically says you can take, modify, share, publish this as much as you
-// want, as long as you don't make it closed source.
-//
-// If you do find it useful, we'd love to hear about it! Check us out at
-// http://robotcasserole.org/ and leave us a message!
-///////////////////////////////////////////////////////////////////////////////
-
-/**
- * Class which implements a simple discrete-time integral calculation. Output is equal
- * to the accumulation of the input signal in units of <signal> * second. 
- *
- * There are many ways to take an integral in discrete time, this method in the
- * future will support many of them.
- */
 
 public class IntegralCalculator {
 	private double prev_time;
 	private double accumulator;
+	private double[] point_num;
+	private int choice;
+	private double value;
 	
-	/**
-	 * Constructor for integral calculator. Note that you should instantiate one of these classes per signal we wish to take the integral of.
-	 */
-	public IntegralCalculator(){
+	IntegralCalculator(int choice_in){
 		prev_time = Timer.getFPGATimestamp();
 		accumulator = 0;
+		point_num = new double [5];
+		choice = choice_in;
 	}
 	
-	/**
-	 * Given a new input value, output its integral based on the last time the
-	 * method was invoked. The idea is you would read a signal, then call this method
-     * with this new signal value, so you have the integral right there. Then use it 
-     * later on. Presently, this method only supports a simple "sample & hold" integral
-     * calculation where the signal is assumed constant between the previous sample and this
-     * sample. In the future, cubic-spline methods will be supported
-     *
-	 * @param in input to take the integral of
-	 * @return the integral of the signal "in" in units of in * sec.
-	 */
+	
 	public double calcIntegral(double in){
 		double cur_time = Timer.getFPGATimestamp();
 		/*Integration method - "Sample and hold"*/
-		/*TBD - other methods...*/
-		accumulator += in * (cur_time - prev_time);
+		point_num[4] = point_num[3];
+		point_num[3] = point_num[2];
+		point_num[2] = point_num[1];
+		point_num[1] = point_num[0];
+		point_num[0] = in;
 		
+		if (choice == 0) /*trapezoid rule*/{
+			value = ((cur_time-prev_time)/2)*(point_num[0] + point_num[1]);
+		}
+		if (choice == 1) /*simpson's rule*/{
+			value = (1/2)*((cur_time-prev_time)/6)*(point_num[0] + (4*point_num[1]) + point_num[2]);
+		}
+		if (choice == 2) /*simpson's 3/8 rule*/{
+			value = (1/3)*((cur_time-prev_time)/8)*(point_num[0] + (3*point_num[1]) + (3*point_num[2])+ point_num[3]);
+		}
+		if (choice == 3) /*boole's rule*/{
+			value = (1/4)*((cur_time-prev_time)/90)*((7*point_num[0]) + (32*point_num[1]) + (12*point_num[2])+ (32*point_num[3]) + (7 * point_num[4]));
+		}
+		if (choice == 4) /*rectangular rule*/ {
+			value += in * (cur_time - prev_time);
+		}
 		/*Save values for next loop and return*/
+		accumulator = accumulator + value;
 		prev_time = cur_time;
 		return accumulator;
 	}
 	
-	/**
-	 * Reset the integration value back to zero immediately. Useful for
-     * returning the accumulator back to an initial state.
-     */
 	public void resetIntegral(){
 		accumulator = 0;
 	}
