f2ef132454b7a70274a356320f59295b07bb3e41
Chris Gerth <chrisgerth010592@gmail.com>
2016-07-21 19:31:01 -0500

    updated signal plotter, but the octave part was choking on large signals.
    Added viewer2, which is html/javascriptbased and does better. Still a bit buggy, and need presets.

--- DataViewer/plotSignals.bat
+++ DataViewer/plotSignals.bat
@@ -4,8 +4,14 @@ REM and that test gui script is located in this directory
 
 IF EXIST B:\ GOTO :RUN_FROM_B_DRIVE
 
+IF EXIST D:\ GOTO :RUN_FROM_D_DRIVE
+
 C:\Octave\Octave-4.0.0\octave.vbs --no-gui --persist genPlots.m
 exit
 
 :RUN_FROM_B_DRIVE
 B:\Octave\Octave-4.0.0\octave.vbs --no-gui --persist genPlots.m
+
+
+:RUN_FROM_D_DRIVE
+D:\Octave\Octave-4.0.0\octave.vbs --no-gui --persist genPlots.m
